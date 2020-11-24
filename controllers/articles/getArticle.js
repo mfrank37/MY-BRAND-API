b427e6c25ad7ca1b51e07a926165863b25a4021d
Mungeri Frank <mungerifrank@gmail.com>
2020-11-24 19:15:08 +0200

    feat(get-article): implement READ article with control free from routes

--- controllers/articles/getArticle.js
+++ controllers/articles/getArticle.js
@@ -1,16 +1,26 @@
+const Article = require('../../models/articleModel');
+
 const getArticle = (req, res) => {
-    let found = false;
-    const article = {};
-    if (found) {
-        res.send(article);
-    }
-    // get articles if not continue below
-    res.status(404);
-    res.json({
-        code: 'not-found',
-        message: `Server can not find requested article : ${req.url}`
+  Article.findById(req.url.slice(1))
+    .then(ARTICLE => {
+      if (!ARTICLE) {
+        res.status(404).send({
+          code: 'not-found',
+          message: `Server can not find requested article : ${req.url}, It may have been deleted.`,
+        });
+      } else {
+        res.status(200).send(ARTICLE)
+      }
+    })
+    .catch(err => {
+      res.status(400);
+      res.json({
+        code: 'bad-request',
+        message: `Server can not process request which is invalid : ${req.url}`,
+        mongoError: err
+      });
+      res.send();
     });
-    res.send();
 }
 
 module.exports = getArticle;
