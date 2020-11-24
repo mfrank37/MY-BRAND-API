9cce1e3f6b42f0d31ecd63cd819377d87d1f336e
Mungeri Frank <mungerifrank@gmail.com>
2020-11-24 19:33:37 +0200

    feat(update-article): implement UPDATE with control separate from route

--- controllers/articles/updateArticle.js
+++ controllers/articles/updateArticle.js
@@ -1,22 +1,38 @@
+const Article = require('../../models/articleModel');
+
 const updateArticle = (req, res) => {
-    let found = true;
-    if (found) {
-      res.status(200);
-      res.json({
-        code: 'update',
-        message: 'updating succeeded for the article',
-        articleID: 'viziyo-twenti-twenti',
-        article: req.body
+  Article.findOne({
+      _id: req.url.slice(1)
+    })
+    .then(ARTICLE => {
+      ARTICLE.title = req.body.title;
+      ARTICLE.description = [...req.body.description];
+      ARTICLE.coverImageURL = req.body.coverImageURL;
+      ARTICLE.updated = new Date();
+      ARTICLE.save()
+        .then(UPDATED => {
+          res.status(200).json({
+            code: 'updated',
+            message: 'the article was updated successfully',
+            article: UPDATED
+          });
+          res.send();
+        })
+        .catch(err => {
+          res.status(500).send({
+            code: 'not-updated',
+            message: 'Unexpected error caught while updating article. Article may have been deleted.',
+            mongoError: err
+          })
+        });
+    })
+    .catch(err => {
+      res.status(400).send({
+        code: 'bad-request',
+        message: 'Bad request. Please ensure the article\'s fields and id in url are all valid',
+        mongoError: err
       });
-      res.send();
-    } else {
-      res.status(404);
-      res.json({
-        code: 'not-found',
-        message: `Server can not find requested article : ${req.url}`
-      });
-      res.send();
-    }
-  }
+    });
+}
 
 module.exports = updateArticle;
