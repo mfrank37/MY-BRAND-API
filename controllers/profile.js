29eadd0c764e71e756ce9de69a8969aaaccf31e7
Mungeri Frank <mungerifrank@gmail.com>
2020-11-25 15:16:27 +0200

    feat(profile): implement UPDATE for profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,7 +1,6 @@
 const Profile = require('../models/profileModel');
 
 const getProfile = (req, res) => {
-  console.log(process.env.profileID);
   Profile.findById(process.env.profileID)
     .then(PROFILE => {
       if (PROFILE) {
@@ -21,8 +20,38 @@ const getProfile = (req, res) => {
     })
 }
 
-const updateProfile = () => {
-
+const updateProfile = (req, res) => {
+  Profile.findOne({
+      _id: process.env.profileID
+    })
+    .then(PROFILE => {
+      PROFILE.names = req.body.names   || PROFILE.names;
+      PROFILE.about = req.body.about   || PROFILE.about;
+      PROFILE.avatar = req.body.avatar || PROFILE.avatar;
+      PROFILE.email = req.body.email   || PROFILE.email;
+      PROFILE.save()
+        .then(UPDATED => {
+          res.status(200).json({
+            code: 'updated',
+            message: 'the profile was updated successfully'
+          });
+          res.send();
+        })
+        .catch(err => {
+          res.status(500).send({
+            code: 'not-updated',
+            message: 'Unexpected error caught while updating profile. Either some fields have invalid data or user profile have been deleted.',
+            mongoError: err
+          })
+        });
+    })
+    .catch(err => {
+      res.status(400).send({
+        code: 'bad-request',
+        message: 'Bad request. You may not have permission to update profile.',
+        mongoError: err
+      });
+    });
 }
 
 module.exports = {
