c56d087df0285376b9717a661808d9f601e782b0
Mungeri Frank <mungerifrank@gmail.com>
2020-12-04 11:14:18 +0200

    fix: add PORT and listen to it

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const badRequest = require('./controllers/badRequest');
 require('dotenv').config();
 require('./DB/connection');
 
-const PORT = Number(process.env.PORT);
+let PORT = process.env.PORT || 3000;
 
 // Handle req.body formats and api routes
 const app = express();
@@ -13,5 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api', routes);
 app.use('/', badRequest);
+
+app.listen(PORT);
 
 module.exports = app;
